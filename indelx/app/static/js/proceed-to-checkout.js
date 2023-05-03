769b65b5a3d921c31d514bb86a5e36cca6970b99
prajjwal <acharyaprajjwal152@gmail.com>
2023-05-03 22:32:10 +0545

    js reverted

--- indelx/app/static/js/proceed-to-checkout.js
+++ indelx/app/static/js/proceed-to-checkout.js
@@ -1,38 +1,31 @@
+const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+const checkout_amount = document.querySelector('.checkoutamount').textContent;
+console.log(checkout_amount);
 
-//const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-   window.addEventListener("load", function(){
-  // Code that runs after the page has loaded
-
-    checkout_amount=document.querySelector('.checkoutamount').textContent;
-    console.log(checkout_amount);
-
-    function checkout() {
-
-
-
-     var cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
-     console.log(cartItems);
+function checkout() {
+  const cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+  console.log(cartItems);
 
   // Send AJAX request to checkout view
-       $.ajax({
-        url: '/validate_user/',
-        type: 'POST',
-        contentType: 'application/json',
-        dataType: 'json',
-        data: JSON.stringify({cartItems: cartItems}),
-        headers: {
-            'X-CSRFToken': csrfToken
-        },
-          success: function(response) {
-            if (response.authenticated) {
-              window.location.href = '/place_order';
-            } else {
-                window.location.href = '/login';
-            }
-          },
-          error: function(jqXHR, textStatus, errorThrown) {
+  $.ajax({
+    url: '/validate_user/',
+    type: 'POST',
+    contentType: 'application/json',
+    dataType: 'json',
+    data: JSON.stringify({cartItems}),
+    headers: {
+      'X-CSRFToken': csrfToken
+    },
+    success: function(response) {
+      if (response.authenticated) {
+        window.location.href = '/place_order';
+      } else {
+        window.location.href = '/login';
+      }
+    }
+  });
+}
 
-          }
-          });
-    };
-    });
+window.addEventListener("load", function(){
+  // Code that runs after the page has loaded
+});
