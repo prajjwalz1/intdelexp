640793472fb27f1e2e29f5d55196bd3b73ebf1d6
prajjwal <acharyaprajjwal152@gmail.com>
2023-05-03 13:18:34 +0545

    transparency changed

--- indelx/app/static/js/proceed-to-checkout.js
+++ indelx/app/static/js/proceed-to-checkout.js
@@ -1,37 +1,37 @@
- console.log('{{csrf_token}}');
-const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-    window.onload = function() {
-  // Code that runs after the page has loaded
-
-    checkout_amount=document.querySelector('.checkoutamount').textContent;
-    console.log(checkout_amount);
-    }
-    function checkout() {
-
-
-
-     var cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
-     console.log(cartItems);
-
-  // Send AJAX request to checkout view
-       $.ajax({
-        url: '/validate_user/',
-        type: 'POST',
-        contentType: 'application/json',
-        dataType: 'json',
-        data: JSON.stringify({cartItems: cartItems}),
-        headers: {
-            'X-CSRFToken': csrfToken
-        }
-          success: function(response) {
-            if (response.authenticated) {
-              window.location.href = '/place_order';
-            } else {
-                window.location.href = '/login';
-            }
-          },
-          error: function(jqXHR, textStatus, errorThrown) {
-                 console.log(response);
-          }
-          });
-    }
+// console.log('{{csrf_token}}');
+//const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+//    window.onload = function() {
+//  // Code that runs after the page has loaded
+//
+//    checkout_amount=document.querySelector('.checkoutamount').textContent;
+//    console.log(checkout_amount);
+//    }
+//    function checkout() {
+//
+//
+//
+//     var cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+//     console.log(cartItems);
+//
+//  // Send AJAX request to checkout view
+//       $.ajax({
+//        url: '/validate_user/',
+//        type: 'POST',
+//        contentType: 'application/json',
+//        dataType: 'json',
+//        data: JSON.stringify({cartItems: cartItems}),
+//        headers: {
+//            'X-CSRFToken': csrfToken
+//        }
+//          success: function(response) {
+//            if (response.authenticated) {
+//              window.location.href = '/place_order';
+//            } else {
+//                window.location.href = '/login';
+//            }
+//          },
+//          error: function(jqXHR, textStatus, errorThrown) {
+//                 console.log(response);
+//          }
+//          });
+//    }
