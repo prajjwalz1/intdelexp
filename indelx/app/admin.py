55923c838b968bfed636641aacb097f478b4165f
prajjwal <acharyaprajjwal152@gmail.com>
2023-05-07 17:36:59 +0545

    2 more images added

--- indelx/app/admin.py
+++ indelx/app/admin.py
@@ -1,17 +1,17 @@
 from django.contrib import admin
-from.models import slide,featbanner,product_model,CartItem,MyUser,Customer,OrderItem,Order,PendingOrder
+from.models import slide,featbanner,product_model,CartItem,MyUser,Customer,OrderItem,Order,PendingOrder,product_category
 # Register your models here.
 admin.site.register(slide)
 admin.site.register(featbanner)
 admin.site.register(MyUser)
+admin.site.register(product_category)
 
 
 admin.site.register(Customer)
 # admin.site.register(CartItem)
 
 
-admin.site.register(OrderItem)
-admin.site.register(Order)
+
 admin.site.register(product_model)
 
 
--- indelx/app/models.py
+++ indelx/app/models.py
@@ -59,7 +59,8 @@ class featbanner(models.Model):
 
     def __str__(self):
         return self.title;
-
+class product_category(models.Model):
+    name=models.CharField(max_length=100)
 class product_model(models.Model):
     pname=models.CharField(max_length=100)
     product_id= models.AutoField
@@ -67,10 +68,12 @@ class product_model(models.Model):
     discounted_price=models.FloatField()
     description=models.TextField()
     brand=models.CharField(max_length=100)
+    category = models.ForeignKey(product_category, on_delete=models.CASCADE)
     CATEGORY_CHOICES = (
         ('BW', 'Buttom wear'),
         ('TW', 'Top Wear'),
-        ('Fw', 'Footwear')
+        ('Fw', 'Footwear'),
+
 
     )
     category=models.CharField(choices=CATEGORY_CHOICES,max_length=2)
