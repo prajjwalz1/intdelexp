ecbe1fe345ca8e6dff777efa475cb31ff419d33b
prajjwal <acharyaprajjwol152@gmail.com>
2023-05-15 09:18:00 +0545

    removed import of image cropping

--- indelx/indelx/settings.py
+++ indelx/indelx/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 from django.contrib.auth import authenticate,login
 from pathlib import Path
-import image_cropping
+
 from django.urls import path
 import os
 import logging
