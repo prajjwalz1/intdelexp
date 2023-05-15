2d08f39c3b8b97d67c6f83fe93806c80b568563f
prajjwal <acharyaprajjwal152@gmail.com>
2023-05-15 03:24:54 +0000

    new added

---
+++ indelx/static/js/proceed-to-checkout.js
@@ -0,0 +1,32 @@
+const checkout_amount = document.querySelector('.checkoutamount').textContent;
+console.log(checkout_amount);
+
+function checkout(event) {
+  event.preventDefault();
+  const cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+  console.log(cartItems);
+
+  // Send AJAX request to checkout view
+  $.ajax({
+    url: '/validate_user/',
+    type: 'POST',
+    contentType: 'application/json',
+    dataType: 'json',
+    data: JSON.stringify({cartItems}),
+    headers: {
+      'X-CSRFToken': csrfToken
+    },
+    success: function(response) {
+      if (response.authenticated) {
+        window.location.href = '/place_order';
+      } else {
+        window.location.href = '/login';
+      }
+    }
+  });
+  return false;
+}
+
+window.addEventListener("load", function(){
+  // Code that runs after the page has loaded
+});
